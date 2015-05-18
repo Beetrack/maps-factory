eaeb912efd97628b3714a13d849ad44140f2751c
Antonio Grass <antonio_grass@hotmail.com>
2015-05-18 16:57:42 -0300

    fix

--- src/maps_factory.js
+++ src/maps_factory.js
@@ -14,16 +14,16 @@ function MapsFactory ( options ) {
 };
 
 MapsFactory.prototype.distanceBetween = function(waypoint1, waypoint2) {
-  var = rm = 6371*1000; //radius in meters
+  var rm = 6371*1000; //radius in meters
   var rad_per_deg = Math.PI/180;
   var lat1 = waypoint1[0]*rad_per_deg;
   var lng1 = waypoint1[1]*rad_per_deg;
   var lat2 = waypoint2[0]*rad_per_deg;
   var lng2 = waypoint2[1]*rad_per_deg;
-  var dlat = lat2 - lat1
-  var dlng = lng2 - lng1
-  var a = (dlat/2)*2 + Math.cos(lat1) * Math.cos(lat2) * (dlon/2)*2
-  var c = 2 * Math.asin(Math.sqrt(a))
+  var dlat = lat2 - lat1;
+  var dlng = lng2 - lng1;
+  var a = (dlat/2)*2 + Math.cos(lat1) * Math.cos(lat2) * (dlng/2)*2;
+  var c = 2 * Math.asin(Math.sqrt(a));
   return rm * c //Delta in meters
 };
 
