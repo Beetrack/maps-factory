e0969b7f8ff57ae619247652e56857f65d8d0250
Andres Colonia <andres@andres.local>
2015-05-18 12:43:22 -0300

    add mapbox script with access token

--- src/map_box.js
+++ src/map_box.js
@@ -3,4 +3,6 @@ function MapBox( options ) {
   // some defaults
   this.options.div = options.div || "#maps";
 
+  L.mapbox.accessToken = options.token;
+  this.map = L.mapbox.map('map', 'mapbox.streets').setView([options.lat, options.lng], options.zoom);
 }
