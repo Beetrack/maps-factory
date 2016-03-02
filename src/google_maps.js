9daa01642c1d0955379ad1b3b4afd1090e4388ca
jocodinan <jorge.codina@gmail.com>
2016-03-02 18:10:35 -0300

    disable controls option on gmaps

--- src/google_maps.js
+++ src/google_maps.js
@@ -14,11 +14,13 @@ function GoogleMaps( options ) {
   this.options.lat = options.lat || 0;
   this.options.lng = options.lng || 0;
   this.options.styles = options.styles || false;
+  this.options.mapTypeControl = options.mapTypeControl || false;
 
   this.mapOptions = {
     zoom: this.options.zoom,
     center: new google.maps.LatLng(this.options.lat, this.options.lng),
-    styles: this.options.styles
+    styles: this.options.styles,
+    mapTypeControl: this.options.mapTypeControl
   };
 
   this.map = new google.maps.Map(getElementById(this.options.div), this.mapOptions);
